866fb1e995fac314e86fc2aba1cded3804fe161f
dinghr <dinghr537@gmail.com>
2021-05-16 22:43:45 +0800

    second commit

--- README.md
+++ README.md
@@ -1,60 +1,30 @@
 # DSAI-HW3-2021
 
-### Source
+## 檔案說明
 
-  - [Slide](https://docs.google.com/presentation/d/1JW27_5HXYZhqWmgvDhtXBaFTOfksO_dS/edit#slide=id.p1)
-  - [Dashboard](https://docs.google.com/spreadsheets/d/1cjhQewnXT2IbmYkGXRYNC5PlGRafcbVprCjgSFyDAaU/edit?pli=1#gid=0)
+- main.py	為主程式，負責讀入前7天的data，並使用模型進行預測。
+- night.h5    為訓練出來的lstm模型，用於預測接下來一天的產電耗電量
+- pipfile, pipfile.lock   版本控制
 
-### Rules
+- training.ipynb / training.py   為用於讀取訓練資料，訓練處night.h5模型用的筆記本/程式
 
-- SFTP
+## 模型說明
 
+第一次訓練模型時，用上了時間，耗電量，產電量三類data，用前7天的data預測接下來一整天的產電耗電量。模型選擇是LSTM。然而，做出來之後測試發現，效果並不好，甚至不如什麼投標都不做的收益高。於是拋棄了此模型。
+
+第二次訓練，針對每個小時單獨訓練模型，針對各個小時，用前七天的對應時間的data，猜測新一天的對應時間的產電耗電量。效果不錯，然而由於模型有點多，加載時間實在是太長，無法在限制的時間內完成運行，於是又拋棄了這個做法。
+
+第三次仔細觀察了數據，發現白天預測的準確率不佳，但是夜晚以及早上由於產電量總是會低於耗電量，有一個比較明顯的特徵，不容易出錯。因此單獨訓練晚上6點到早上6點的模型，用於預測需要買多少電比較合適。訓練時，將產電量與耗電量相減，得到電量差值，直接用前7天的電量差值，預測後一天的電量差，模型小，效果也不錯。就這樣定了下來，白天選擇先不預測，晚上選擇用模型預測需要買多少電，然後穩妥起見，將預測得到的數值向下取整，得到最終要買的電的量。
+
+## 執行main.py
+
+只需
+
+```shell
+python main.py --cunsumption [path] --generation [path] --bidresult [path] --output [path]
+# 四者對應的默認值分別為 ./sample_data/consumption.csv
+#                    ./sample_data/generation.csv
+#                    ./sample_data/bidresult.csv
+#                    output.csv
 ```
 
-┣━ upload/
-┗━ download/
-   ┣━ information/
-   ┃  ┗━ info-{mid}.csv
-   ┣━ student/
-   ┃  ┗━ {student_id}/
-   ┃     ┣━ bill-{mid}.csv
-   ┃     ┗━ bidresult-{mid}.csv
-   ┗━ training_data/
-      ┗━ target{household}.csv  
-      
-```
-
-1. `mid` 為每次媒合編號
-2. `household` 為住戶編號，共 50 組
-3. 請使用發給組長的帳號密碼，將檔案上傳至 `upload/`
-4. 相關媒合及投標資訊皆在 `download/` 下可以找到，可自行下載使用
-
-
-- File
-
-```
-
-┗━ {student_id}-{version}.zip
-   ┗━ {student_id}-{version}/
-      ┣━ Pipfile
-      ┣━ Pipfile.lock
-      ┣━ main.py
-      ┗━ {model_name}.hdf5
-
-```
-
-1. 請務必遵守上述的架構進行上傳 (model 不一定要有)
-2. 檔案壓縮請使用 `zip`，套件管理請使用 `pipenv`，python 版本請使用 `3.8`
-3. 檔名：{學號}-{版本號}.zip，例：`E11111111-v1.zip`
-4. 兩人一組請以組長學號上傳
-5. 傳新檔案時請往上加版本號，程式會自動讀取最大版本
-6. 請儲存您的模型，不要重新訓練
-
-- Bidding
-
-1. 所有輸入輸出的 csv 皆包含 header
-2. 請注意輸入的 `bidresult` 資料初始值為空
-3. 輸出時間格式為 `%Y-%m-%d %H:%M:%S` ，請利用三份輸入的 data 自行選一份，往後加一天即為輸出時間  
-   例如: 輸入 `2018-08-25 00:00:00 ~ 2018-08-31 23:00:00` 的資料，請輸出 `2018-09-01 00:00:00 ~ 2018-09-01 23:00:00` 的資料(一次輸出`一天`，每筆單位`一小時`)
-4. 程式每次執行只有 `120 秒`，請控制好您的檔案執行時間
-5. 每天的交易量限制 `100 筆`，只要有超出會全部交易失敗，請控制輸出數量
--- main.py
+++ main.py
@@ -35,41 +35,45 @@ def output(path, data):
 if __name__ == "__main__":
     args = config()
 
-    # read model
-    model = load_model('./night.h5')
+    try:
+        # read model
+        model = load_model('./night.h5')
 
-    consumption = pd.read_csv(args.consumption)
-    generation = pd.read_csv(args.generation)
+        consumption = pd.read_csv(args.consumption)
+        generation = pd.read_csv(args.generation)
 
-    if generation.shape[0] < 168:
+        if generation.shape[0] < 168:
+            data = []
+            output(args.output, data)
+            quit()
+            # data not enough
+
+        from datetime import datetime, timedelta
+        yesterday = datetime.strptime(generation.iloc[-1]['time'], '%Y-%m-%d %H:%M:%S')
+        today = yesterday + timedelta(days=1)
+
+        # generation.iloc[-1]['time']
+        # a + timedelta(days=1)
+
+        generation['buy'] = pd.DataFrame({'buy':(generation['generation'] - consumption['consumption'])})
+        generation['time'] = generation['time'].map(lambda x: int(x[-8:-6]))
+        
+        hour0 = generation[generation['time']==0]['buy'].to_numpy().reshape(-1, 7, 1)
+        ans = model.predict(hour0)
+
         data = []
-        output(args.output, data)
-        quit()
-        # data not enough
+        hours = [0, 1, 2, 3, 4, 5, 6, 18, 19, 20, 21, 22, 23]
+        for i in hours:
+            _data = generation[generation['time']==i]['buy'].to_numpy().reshape(-1, 7, 1)
+            ans = model.predict(_data)
+            print(f'time = {i} and ans = {ans}')
+            if ans[0][0] < 0:
+                cur_time = today.replace(hour=i)
+                data.append([cur_time.strftime('%Y-%m-%d %H:%M:%S'), "buy", 2.2, floor(-ans[0][0])])
 
-    from datetime import datetime, timedelta
-    yesterday = datetime.strptime(generation.iloc[-1]['time'], '%Y-%m-%d %H:%M:%S')
-    today = yesterday + timedelta(days=1)
-
-    # generation.iloc[-1]['time']
-    # a + timedelta(days=1)
-
-    generation['buy'] = pd.DataFrame({'buy':(generation['generation'] - consumption['consumption'])})
-    generation['time'] = generation['time'].map(lambda x: int(x[-8:-6]))
-    
-    hour0 = generation[generation['time']==0]['buy'].to_numpy().reshape(-1, 7, 1)
-    ans = model.predict(hour0)
-
-    data = []
-    hours = [0, 1, 2, 3, 4, 5, 6, 18, 19, 20, 21, 22, 23]
-    for i in hours:
-        _data = generation[generation['time']==i]['buy'].to_numpy().reshape(-1, 7, 1)
-        ans = model.predict(_data)
-        print(f'time = {i} and ans = {ans}')
-        if ans[0][0] < 0:
-            cur_time = today.replace(hour=i)
-            data.append([cur_time.strftime('%Y-%m-%d %H:%M:%S'), "buy", 2.2, floor(-ans[0][0])])
-
+    except:
+        print("CATCH SOMETHING")
+        data = []
 
 
 
